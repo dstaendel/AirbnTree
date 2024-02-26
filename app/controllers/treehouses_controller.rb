25db39d7c3352a1af763ebed02e0b18d6e4404a7
jerome serriere <jerome.serriere@posteo.de>
2024-02-26 16:53:00 +0100

    in treehouse controller added index and show and new and create method

--- app/controllers/treehouses_controller.rb
+++ app/controllers/treehouses_controller.rb
@@ -1,4 +1,33 @@
 class TreehousesController < ApplicationController
+  def index
+    @treehouses = Treehouse.all
+  end
 
+  def show
+    @treehouse = Treehouse.find(params[:id])
+  end
 
+  def new
+    @treehouse = Treehouse.new
+  end
+
+  def create
+    @treehouse = Treehouse.new(treehouse_params)
+
+    if @treehouse.save
+      redirect_to treehouses_path, notice: "Your new Treehouse was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  # def set_restaurant
+  #   @restaurant = Restaurant.find(params[:id])
+  # end
+
+  def treehouse_params
+    params.require(:treehouse).permit(:tree_type, :total_occupancy, :summary, :address, :price, :latitude, :longitude)
+  end
 end
--- app/models/treehouse.rb
+++ app/models/treehouse.rb
@@ -1,3 +1,16 @@
 class Treehouse < ApplicationRecord
   has_many :bookings
+  has_many :users, through: :bookings
+
 end
+
+
+t.string "tree_type"
+t.integer "total_occupancy"
+t.text "summary"
+t.string "address"
+t.integer "price"
+t.float "latitude"
+t.float "longitude"
+t.datetime "created_at", null: false
+t.datetime "upd
