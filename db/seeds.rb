422256ed0868e22264814722deef3a6aac27b718
jerome serriere <jerome.serriere@posteo.de>
2024-02-27 10:29:25 +0100

    created empty showview for treehouses

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,8 +4,8 @@ Treehouse.destroy_all
 
 
 puts "creating ..."
-Treehouse.new(name: "Treehouse of Horror", tree_type: "Oak tree", total_occupancy: 5, summary: "Simpsons themed treehouse", address: "Springfield", price: 100)
-Treehouse.new(name: "Cabin in the woods", tree_type: "Redwood tree", total_occupancy: 5, summary: "Haunted treehouse", address: "Vancouver", price: 100)
+Treehouse.create(name: "Treehouse of Horror", tree_type: "Oak tree", total_occupancy: 5, summary: "Simpsons themed treehouse", address: "Springfield", price: 100)
+Treehouse.create(name: "Cabin in the woods", tree_type: "Redwood tree", total_occupancy: 5, summary: "Haunted treehouse", address: "Vancouver", price: 100)
 
 
 puts "done creating"
