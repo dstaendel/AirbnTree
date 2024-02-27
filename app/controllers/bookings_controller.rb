c6eae707fd4baaaae187b27226dfff9841042bef
jerome serriere <jerome.serriere@posteo.de>
2024-02-27 15:44:02 +0100

    created new create edit update and destroy method

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,44 @@
 class BookingsController < ApplicationController
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+
+    if @booking.save
+      redirect_to bookings_path, notice: "Your new booking was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @booking = Booking.find(params[:id])
+  end
+
+  def update
+    @booking = Booking.find(params[:id])
+    @booking.update(booking_params)
+    @booking.save
+    redirect_to bookings_path(@booking.id)
+
+  end
+
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to bookings_path, status: :see_other
+  end
+
+  private
+
+  # def set_booking
+  #   @booking = Booking.find(params[:id])
+  # end
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :price, :user_id, :treehouse_id)
+  end
 end
