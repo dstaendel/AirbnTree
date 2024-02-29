ec03dc1d57d6656665980da88c2080c0381158b2
jerome serriere <jerome.serriere@posteo.de>
2024-02-29 13:04:31 +0100

    AddUserToTreehouses user:references, changed seed-file, fixed booking code, fixed profile page, adjusted profile view booking

--- app/models/treehouse.rb
+++ app/models/treehouse.rb
@@ -1,7 +1,7 @@
 class Treehouse < ApplicationRecord
+  belongs_to :user
+
   has_many :bookings
   has_many :users, through: :bookings
-  has_many_attached :photos
-
   has_many_attached :photos
 end
